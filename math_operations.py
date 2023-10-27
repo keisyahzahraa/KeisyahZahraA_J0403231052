6d9d9f249caacba29cc6726f501057c8eeaacb1f
keisyahzahraa <146173424+keisyahzahraa@users.noreply.github.com>
2023-10-27 16:39:45 +0700

    math_operations.py

---
+++ math_operations.py
@@ -0,0 +1,16 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    if y == 0:
+        raise ZeroDivisionError("Division by zero is not allowed.")
+    return x / y
