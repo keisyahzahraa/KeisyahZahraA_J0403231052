b36ccd28c4ced74ef8833e657b80e53c2309292c
keisyahzahraa <146173424+keisyahzahraa@users.noreply.github.com>
2023-10-29 19:46:19 +0700

    Add files via upload

---
+++ soal2.py
@@ -0,0 +1,14 @@
+def average(num1: float, num2: float) -> float:
+    """Return the average of num1 and num2.
+    >>> average(10,20)
+    15.0
+    >>> average(2.5, 3.0)
+    2.75
+    """
+    return (num1 + num2) / 2
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
+
